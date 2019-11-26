fd824b7ac5de337e812b49bd72d77082233ee86b
willgraf <7930703+willgraf@users.noreply.github.com>
2019-11-26 13:40:11 -0800

    Update setup.py version for 0.2.4 release

--- setup.py
+++ setup.py
@@ -27,13 +27,13 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='Deepcell_Tracking',
-      version='0.2.3',
+      version='0.2.4',
       description='Tracking cells and lineage with deep learning.',
       author='Van Valen Lab',
       author_email='vanvalenlab@gmail.com',
       url='https://github.com/vanvalenlab/deepcell-tracking',
       download_url='https://github.com/vanvalenlab/'
-                   'deepcell-tracking/tarball/0.2.3',
+                   'deepcell-tracking/tarball/0.2.4',
       license='LICENSE',
       install_requires=['networkx>=2.1',
                         'numpy',
