386d6ef044c16b749bf3e376ca90dfdf48e11af5
William Graf <7930703+willgraf@users.noreply.github.com>
2020-02-11 17:52:52 -0800

    Ignore PEP8 error in setup.py

--- setup.py
+++ setup.py
@@ -45,5 +45,5 @@ setup(name='Deepcell_Tracking',
           'tests': ['pytest',
                     'pytest-pep8',
                     'pytest-cov'],
-      },
+      },  # nopep8
       packages=find_packages())
