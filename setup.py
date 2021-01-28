af0b846c5b2da9560a021be6dc04e17a59990c27
William Graf <7930703+willgraf@users.noreply.github.com>
2021-01-28 10:34:49 -0800

    missing os import

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import os
+
 from codecs import open
 from setuptools import setup
 from setuptools import find_packages
