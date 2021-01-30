f16ce4df3cd8a80d815ca7ce53152488c0d36e02
William Graf <7930703+willgraf@users.noreply.github.com>
2021-01-29 17:00:27 -0800

    Undo change

--- deepcell_tracking/utils.py
+++ deepcell_tracking/utils.py
@@ -24,7 +24,7 @@
 # limitations under the License.
 # ==============================================================================
 """Utilities for tracking cells"""
-# minor change to trigger PR
+
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
