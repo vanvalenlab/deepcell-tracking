dd8ce23b29e08a11509140d4f73f5714bfff3653
William Graf <7930703+willgraf@users.noreply.github.com>
2021-07-01 19:06:52 -0700

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -77,4 +77,5 @@ setup(name=NAME,
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8'])
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9'])
