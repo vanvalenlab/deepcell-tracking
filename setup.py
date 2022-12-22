d86a220600f46705ca9bffef985fcfff24ba3441
Ross Barnowski <rossbar@berkeley.edu>
2022-12-22 12:36:26 -0800

    MAINT: Add python_requires and 3.10 to setup.py

--- setup.py
+++ setup.py
@@ -71,9 +71,11 @@ setup(name=NAME,
       long_description=readme,
       long_description_content_type='text/markdown',
       packages=find_packages(),
+      python_requires='>=3.7, <3.11',
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9'])
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'])
